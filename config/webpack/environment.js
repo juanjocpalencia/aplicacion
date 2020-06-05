df2e2ae3e543b27b94ff9c3a510e1fd9fa8d6f2d
juanjocpalencia <43501446+juanjocpalencia@users.noreply.github.com>
2020-06-04 22:11:14 -0500

    bootstrap ruby
    ROR with BS4

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack =require ('webpack')
+environment.plugin.append('Provide',new webpack.ProvidePlugin({
+    $:'jquery',
+    jQuery:'jquery',
+    Pooper: ['popper.js','default']
+
+}))
 module.exports = environment
